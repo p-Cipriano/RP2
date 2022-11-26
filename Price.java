b5d0af3683bb7f9f05df14b7ce4835f6594291ea
p-Cipriano <cipriano.pedrof@gmail.com>
2022-11-26 18:01:16 -0300

    Commit 14

--- Price.java
+++ Price.java
@@ -1,4 +1,10 @@
 public abstract class Price {
   public abstract int getPriceCode();
   public abstract double getCharge(int daysRented);
+  public int getFrequentRenterPoints(int daysRented) {
+    int frequentRenterPoints = 1;
+    if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+      frequentRenterPoints++;
+    return frequentRenterPoints;
+  }
 }
