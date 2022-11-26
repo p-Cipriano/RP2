d9d2ab599f52d990ed22499deca2f0963a497110
p-Cipriano <cipriano.pedrof@gmail.com>
2022-11-26 17:22:26 -0300

    Commit 13

--- ChildrensPrice.java
+++ ChildrensPrice.java
@@ -2,4 +2,10 @@ public class ChildrensPrice extends Price {
   public int getPriceCode() {
     return Movie.CHILDRENS;
   }
+  public double getCharge(int daysRented) {
+    double thisAmount = 1.5;
+    if (daysRented > 3)
+      thisAmount += (daysRented - 3) * 1.5;
+    return thisAmount;
+  }
 }
--- RegularPrice.java
+++ RegularPrice.java
@@ -2,4 +2,10 @@ public class RegularPrice extends Price {
   public int getPriceCode() {
     return Movie.REGULAR;
   }
+  public double getCharge(int daysRented) {
+    double thisAmount = 2;
+    if (daysRented > 2)
+      thisAmount += (daysRented - 2) * 1.5;
+    return thisAmount;
+  }
 }
